4a2dcb47ecfdbb3c7b35cfd058ed89d3086ee6c3
Egor <e-okishev@mail.ru>
2023-09-10 18:42:53 +0300

    updated

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import "../src/style.css";
-import { dotsArr } from "../src/dotsData.js";
+import { dotsArr } from "./dotsData.js";
 
 const container = document.querySelector(".container");
 const text = document.createElement("p");
